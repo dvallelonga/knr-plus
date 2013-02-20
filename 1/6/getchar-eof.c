255c99a41240a18f29c1fa71ec2d80b09dcb72c7
David Vallelonga <dave@ip-10-122-74-52.ec2.internal>
2013-02-20 01:10:06 +0000

    chap 1 exercises 6 & 7.

---
+++ 1/6/getchar-eof.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* 
+  verify that the expression 
+     getchar != EOF 
+   is 0 or 1. 
+*/
+main()
+{
+  int c;
+  c = getchar() != EOF;
+  printf("getchar() != EOF: %d\n", c);
+}
---
+++ 1/7/eof-value.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+/* print the value of EOF */
+main()
+{
+  printf("value of EOF: %d\n", EOF);
+}
